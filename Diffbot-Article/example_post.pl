01f100ffdacfbfa9656327b0334e7c68fad72af2
Usman Raza <usman.r123@gmail.com>
2014-01-18 01:23:41 +0500

    Post example

--- Diffbot-Article/example_post.pl
+++ Diffbot-Article/example_post.pl
@@ -11,7 +11,7 @@ use Data::Dumper::Concise;
 
 #USER CODE                                 
 my $token = "f2e2d920eab44063aa742b2b0698ab49"; #Set your token here
-my $fedata = articlePost("http://blog.diffbot.com/diffbots-new-product-api-teaches-robots-to-shop-online",$token,['*'],"/home/slingshot/work/tempspace/data","text/html",2,"hello");
+my $fedata = articlePost("http://blog.diffbot.com/diffbots-new-product-api-teaches-robots-to-shop-online",$token,['*'],"/tmp/data","text/html",2,"hello");
 print $fedata;
 my $json_dump = JSON::XS::decode_json($fedata);
 print Dumper $json_dump;
